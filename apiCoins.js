fcd85070d55dca8edd120d560e949cce83afb8f2
Ikarosv <ikarovieiraa@gmail.com>
2022-10-07 20:07:03 -0300

    Requisito 2 feito.

--- apiCoins.js
+++ apiCoins.js
@@ -1,11 +1,27 @@
 const apiData = {
   url: 'https://api.coincap.io/v2/assets',
 };
+const ulCoinsList = document.querySelector('#coins-list');
+console.log(ulCoinsList);
+
+const createLi = (text) => {
+  const li = document.createElement('li');
+  li.innerText = text;
+  return li;
+};
 
 const fetchCoins = async () => {
-  const response = await fetch(apiData.url);
-  const data = await response.json();
-  console.log(data.data);
+  try {
+    const response = await fetch(apiData.url);
+    const data = await response.json();
+    data.data.forEach((coin) => {
+      const price = parseFloat(coin.priceUsd);
+      const liText = `${coin.name} (${coin.symbol}): ${price.toFixed(2)}`;
+      ulCoinsList.appendChild(createLi(liText));
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 fetchCoins();
